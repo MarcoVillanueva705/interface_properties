c4f7a5386901bbd1e0b57c8189ed7cb1dfbd6a1b
marco villanueva <villanuevamarco98@gmail.com>
2020-11-15 17:24:23 -0700

    Add project files.

---
+++ interface_properties/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace interface_properties
+{
+    class Program
+    {
+        //interface IPerson
+        //{
+        //    string Name
+        //    {
+        //        get; set;
+        //    }// abstract property
+            
+        //}
+
+        class Humanoid //: IPerson
+        {
+            private string _name;
+            public string Name  // read-write instance property
+            {
+                get => _name;
+                set => _name = value;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Humanoid myObj = new Humanoid();
+            myObj.Name = "Liam";
+            Console.WriteLine(myObj.Name);
+        }
+    }
+}

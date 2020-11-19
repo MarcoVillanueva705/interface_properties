e8419bcbbcecdcb6e6a434eff75c04585d43de18
marco villanueva <villanuevamarco98@gmail.com>
2020-11-18 20:21:46 -0700

    unit testing with MSTest

---
+++ interface_properties.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("interface_properties.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("interface_properties.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d03a0009-f354-48eb-b00e-67fd898d362a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ interface_properties.UnitTests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace interface_properties.UnitTests
+{
+    [TestClass]
+    public class HumanoidTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+            //every test method has 3 parts:
+            //1.specifies the method under test
+            //2.the scenario being tested
+            //3.the expected behavior
+
+        {   //
+            //Arrange: Where we initialize our objects;
+            
+            //Act: We call the method we're going to test
+
+            //Assert: Verify if the result is correct
+        }
+    }
+}
